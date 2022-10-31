5fe7f1ad42e3fbe91d61dc370bab94a82829b75b
Luinen <lui.peto@gmail.com>
2022-10-31 19:15:38 +0000

    added meta, js: highscore function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,281 +1,154 @@
-const startButton = document.getElementById('start-btn');
-const highScoreButton = document.getElementById('highscore-btn');
-const scoreNumber = document.getElementById ('score');
-const nextButton = document.getElementById('next-btn');
-const questionContainerElement = document.getElementById('question-container');
-const questionElement = document.getElementById('question');
-const answerButtonsElement = document.getElementById('answer-buttons');
-
-let shuffledQuestions, currentQuestionIndex;
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+const progressText = document.getElementById("progressText");
+const scoreText = document.getElementById("score");
+let currentQuestion = {};
+let acceptingAnswers = false;
 let score = 0;
-const SCORE_POINTS = 10;
+let questionCounter = 0;
+let availableQuesions = [];
 
-startButton.addEventListener('click', startGame)
-nextButton.addEventListener('click', () => {
-  currentQuestionIndex++
-  setNextQuestion()
-})
-
-function startGame() {
-  startButton.classList.add('hide')
-  highScoreButton.classList.add('hide')
-  shuffledQuestions = questions.sort(() => Math.random() - .5)
-  currentQuestionIndex = 0
-  questionContainerElement.classList.remove('hide')
-  setNextQuestion()
-}
-
-function setNextQuestion() {
-  resetState()
-  showQuestion(shuffledQuestions[currentQuestionIndex])
-}
-
-function showQuestion(question) {
-  questionElement.innerText = question.question
-  question.answers.forEach(answer => {
-    const button = document.createElement('button')
-    button.innerText = answer.text
-    button.classList.add('btn')
-    if (answer.correct) {
-      button.dataset.correct = answer.correct
-    }
-    button.addEventListener('click', selectAnswer)
-    answerButtonsElement.appendChild(button)
-  })
-}
-
-function resetState() {
-  clearStatusClass(document.body)
-  nextButton.classList.add('hide')
-  while (answerButtonsElement.firstChild) {
-    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
-  }
-}
-
-function selectAnswer(e) {
-  const selectedButton = e.target
-  const correct = selectedButton.dataset.correct
-  setStatusClass(document.body, correct)
-  Array.from(answerButtonsElement.children).forEach(button => {
-    setStatusClass(button, button.dataset.correct)
-  })
-  startButton.style.marginTop = 'auto';
-  startButton.style.fontSize = '1.5rem';
-  if (shuffledQuestions.length > currentQuestionIndex + 1) {
-    nextButton.classList.remove('hide')
-  } else {
-    startButton.innerText = 'Restart'
-    startButton.classList.remove('hide')
-  }
-}
-
-function setStatusClass(element, correct) {
-  clearStatusClass(element)
-  if (correct) {
-    element.classList.add('correct')
-  } else {
-    element.classList.add('wrong')
-  }
-}
-
-function clearStatusClass(element) {
-  element.classList.remove('correct')
-  element.classList.remove('wrong')
-}
-
-const questions = [{
+let questions = [
+  {
     question: 'Which day is Halloween in 2022?',
-    answers: [{
-        text: 'Monday, 31 October',
-        correct: true
-      },
-      {
-        text: 'Sunday, 30 October',
-        correct: false
-      },
-      {
-        text: 'Tuesday, 01 November',
-        correct: false
-      },
-      {
-      text: 'Wednesday, 02 November',
-      correct: false
-      }
-    ]
+    choice1: 'Monday, 31 October',
+    choice2: 'Sunday, 30 October',
+    choice3: 'Tuesday, 01 November',
+    choice4: 'Wednesday, 02 November',
+    answer: 1
   },
   {
     question: 'Where does the term "jack o\'lantern" come from?',
-    answers: [{
-        text: 'from a Swedish village',
-        correct: false
-      },
-      {
-        text: 'from an Irish folktale',
-        correct: true
-      },
-      {
-        text: 'from an Icelandic lake',
-        correct: false
-      },
-      {
-        text: 'None of them',
-        correct: false
-      }
-    ]
+    choice1: 'from a Swedish village',
+    choice2: 'from an Irish folktale',
+    choice3: 'from an Icelandic lake',
+    choice4: 'None of them',
+    answer: 2
   },
   {
     question: 'When was Halloween first celebrated in the United States?',
-    answers: [{
-        text: '1796',
-        correct: false
-      },
-      {
-        text: '1840',
-        correct: true
-      },
-      {
-        text: '1912',
-        correct: false
-      },
-      {
-        text: '1953',
-        correct: false
-      }
-    ]
+    choice1: '1778',
+    choice2: '1840',
+    choice3: '1914',
+    choice4: '1960',
+    answer: 2
   },
   {
     question: 'Why did people start dressing up in Halloween costumes?',
-    answers: [{
-        text: 'because it is fun',
-        correct: false
-      },
-      {
-        text: 'to disguise themselves when the spirits come',
-        correct: false
-      },
-      {
-        text: 'to see the otherworld',
-        correct: false
-      },
-      {
-        text: 'to repel spirits that they believed came back to Earth',
-        correct: true
-      }
-    ]
+    choice1: 'because it is fun',
+    choice2: 'to disguise themselves when the spirits come',
+    choice3: 'to see the otherworld',
+    choice4: 'to repel spirits that they believed came back to Earth',
+    answer: 2
   },
   {
     question: 'What vegetable is part of a tradition on the night before Halloween?',
-    answers: [{
-        text: 'Pepper',
-        correct: false
-      },
-      {
-        text: 'Cucumber',
-        correct: false
-      },
-      {
-        text: 'Cabbage',
-        correct: true
-      },
-      {
-        text: 'Pumpkin',
-        correct: false
-      }
-    ]
-  },{
-    question: 'Why do some people wear clothes inside out on Halloween?',
-    answers: [{
-        text: 'to see a witch at midnight',
-        correct: true
-      },
-      {
-        text: 'to transform into an animal',
-        correct: false
-      },
-      {
-        text: 'to save the planet',
-        correct: false
-      },
-      {
-        text: 'to make a deal with the devil',
-        correct: false
-      }
-    ]
-  },{
-    question: 'What is the most commercially successful horror movie of all time?',
-    answers: [{
-        text: 'Paranormal Activity',
-        correct: false
-      },
-      {
-        text: 'Deliver Us from Evil',
-        correct: false
-      },
-      {
-        text: 'Alien',
-        correct: false
-      },
-      {
-        text: 'It',
-        correct: true
-      }
-    ]
-  },{
-    question: 'What was used before pumpkins to make jack o\'lanterns?',
-    answers: [{
-        text: 'Watermelon',
-        correct: false
-      },
-      {
-        text: 'Potatoes',
-        correct: true
-      },
-      {
-        text: 'Onion',
-        correct: false
-      },
-      {
-        text: 'Beetroot',
-        correct: false
-      }
-    ]
-  },{
-    question: 'What does the black cat symbolize?',
-    answers: [{
-        text: 'Power and strength',
-        correct: false
-      },
-      {
-        text: 'Mystery and secrecy',
-        correct: true
-      },
-      {
-        text: 'Life and death',
-        correct: false
-      },
-      {
-        text: 'Freedom and vanity',
-        correct: false
-      }
-    ]
-  },{
-    question: 'What is the name of the pot witches use to cook up their potions?',
-    answers: [{
-        text: 'Magic cooking pot',
-        correct: false
-      },
-      {
-        text: 'Cauldron',
-        correct: true
-      },
-      {
-        text: 'Black pot',
-        correct: false
-      },
-      {
-        text: 'Wok',
-        correct: false
-      }
-    ]
+    choice1: 'Pepper',
+    choice2: 'Cucumber',
+    choice3: 'Cabbage',
+    choice4: 'Pumpkin',
+    answer: 3
   },
-]
+  {
+    question: 'Why do some people wear clothes inside out on Halloween?',
+    choice1: 'to see a witch at midnight',
+    choice2: 'to transform into an animal',
+    choice3: 'to save the planet',
+    choice4: 'to make a deal with the devil',
+    answer: 1
+  },
+  {
+    question: 'What is the most commercially successful horror movie of all time?',
+    choice1: 'Paranormal Activity',
+    choice2: 'Deliver Us from Evil',
+    choice3: 'Alien',
+    choice4: 'It',
+    answer: 4
+  },
+  {
+    question: 'What was used before pumpkins to make jack o\'lanterns?',
+    choice1: 'Watermelon',
+    choice2: 'Beetroots',
+    choice3: 'Onions',
+    choice4: 'Potatoes',
+    answer: 4
+  },
+  {
+    question: 'What does the black cat symbolize?',
+    choice1: 'Power and strength',
+    choice2: 'Life and death',
+    choice3: 'Mystery and secrecy',
+    choice4: 'Freedom and vanity',
+    answer: 3
+  },
+  {
+    question: 'What is the name of the pot witches use to cook up their potions?',
+    choice1: 'Magic cooking pot',
+    choice2: 'Cauldron',
+    choice3: 'Black pot',
+    choice4: 'Wok',
+    answer: 2
+  }
+];
+
+const CORRECT_BONUS = 100;
+const MAX_QUESTIONS = 10;
+
+function startGame() {
+  questionCounter = 0;
+  score = 0;
+  availableQuesions = [...questions];
+  getNewQuestion();
+}
+
+function getNewQuestion() {
+  if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+    localStorage.setItem("mostRecentScore", score);
+    return window.location.assign("/game_over.html");
+  }
+  questionCounter++;
+  progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+
+  const questionIndex = Math.floor(Math.random() * availableQuesions.length);
+  currentQuestion = availableQuesions[questionIndex];
+  question.innerText = currentQuestion.question;
+
+  choices.forEach(choice => {
+    const number = choice.dataset.number;
+    choice.innerText = currentQuestion["choice" + number];
+  });
+
+  availableQuesions.splice(questionIndex, 1);
+  acceptingAnswers = true;
+}
+
+choices.forEach(choice => {
+  choice.addEventListener("click", e => {
+    if (!acceptingAnswers) return;
+
+    acceptingAnswers = false;
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset.number;
+
+    const classToApply =
+      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+
+    if (classToApply === "correct") {
+      incrementScore(CORRECT_BONUS);
+    }
+
+    selectedChoice.parentElement.classList.add(classToApply);
+
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply);
+      getNewQuestion();
+    }, 1000);
+  });
+});
+
+function incrementScore (number) {
+  score += number;
+  scoreText.innerText = score;
+}
+
+startGame();
